6ee158a6d77b7f6129464580a97d47bde48a65cb
paivaanajulia <116007247+paivaanajulia@users.noreply.github.com>
2023-09-22 10:58:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocaSomPom(){
+document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelector('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+
+// Estrutura de repetição - Enquanto
+while(contador<9)  {
+    
+}
